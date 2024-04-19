575ec674862d29d4509a2f73cb92f3d74dd910dd
tjohann <tjohann@kth.se>
2024-04-20 01:05:11 +0200

    Updated package.json

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.get('/api/keys', async (req, res) => {
     try {
         const { rows } = await pool.query("SELECT * FROM keys;");
+        console.log("Keys successfully collected!");
         res.json(rows);
     } catch (error) {
         console.error('Failed to fetch keys:', error);
