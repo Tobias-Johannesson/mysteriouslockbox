cb7dda6e2b58881c5f25862ad182947d63657847
tjohann <tjohann@kth.se>
2024-04-20 15:54:49 +0200

    Better error messages for server calls

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,6 +12,11 @@ function App() {
         try {
             const apiBaseUrl = process.env.REACT_APP_API_URL;
             const response = await fetch(`${apiBaseUrl}/keys`);
+            if (!response.ok) throw new Error('Network response was not ok');
+            const contentType = response.headers.get("content-type");
+            if (!contentType || !contentType.includes("application/json")) {
+                throw new TypeError("Oops, we haven't got JSON!");
+            }
             const jsonData = await response.json();
             setKeys(jsonData);
         } catch (err) {
