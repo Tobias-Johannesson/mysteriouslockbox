8f951b69b6dcd776c49b55b6ecccb04d9d27bb2c
tjohann <tjohann@kth.se>
2024-04-20 14:32:16 +0200

    Update server with HTTPS

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
+require('dotenv').config();
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const pool = require('./db');
 const app = express();
 
-app.use(cors());
+// CORS options
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN, // Allow only your React app domain, adjust as needed
+  optionsSuccessStatus: 200,
+  methods: "GET, POST, PUT, DELETE"
+};
+
+app.use((req, res, next) => {
+  if (req.secure || process.env.NODE_ENV === 'development') {
+    next();
+  } else {
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+});
+app.use(cors(corsOptions));
 app.use(express.json()); // Middleware to parse JSON bodies
 
 // Endpoint to fetch data
@@ -33,7 +51,18 @@ app.post('/api/unlock', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// Start HTTP or HTTPS server based on the environment
+if (process.env.NODE_ENV === 'production') {
+  const options = {
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+      ca: fs.readFileSync(process.env.SSL_CA_PATH)
+  };
+  https.createServer(options, app).listen(443, () => {
+      console.log('HTTPS server running on port 443');
+  });
+} else {
+  http.createServer(app).listen(3001, () => {
+      console.log('HTTP server running on port 3001');
+  });
+}
